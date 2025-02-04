4b47c942f0a80b83c2cdc012c241fa2d29f369c3
Cormilo <gitgitgithub@yandex.ru>
2025-02-04 15:47:38 +0000

    Попытка добавить видео в качестве фона

--- src/base-container/components/image-layout/ExtraSmallLayout.jsx
+++ src/base-container/components/image-layout/ExtraSmallLayout.jsx
@@ -4,6 +4,8 @@ import { getConfig } from '@edx/frontend-platform';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { Hyperlink, Image } from '@openedx/paragon';
 
+import backgroundVideo from './background.mp4'
+
 import messages from './messages';
 
 const ExtraSmallLayout = () => {
@@ -11,8 +13,23 @@ const ExtraSmallLayout = () => {
 
   return (
     <span
-      className="w-100 bg-primary-500 banner__image extra-small-layout"
-      style={{ backgroundImage: `url(${getConfig().BANNER_IMAGE_EXTRA_SMALL})` }}
+    <video
+    autoPlay
+    loop
+    muted
+    style={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+      transform: 'translate(-50%, -50%)',
+      zIndex: -1,
+    }}
+  >
+    <source src={`url(${backgroundVideo})`} type="video/mp4" />
+  </video>
     >
       <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
         <Image className="company-logo" alt={getConfig().SITE_NAME} src={getConfig().LOGO_WHITE_URL} />
